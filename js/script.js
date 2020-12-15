494956464850d95ef103d518c4c2fb815909e5e8
Pradyumn1832 <69574137+Pradyumn1832@users.noreply.github.com>
2020-12-15 09:10:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     // Show loading icon inside element identified by 'selector'.
     var showLoading = function (selector) {
         var html = "<div class='text-center'>";
-        html += "<img src='img/ajax-loader.gif'></div>";
+        html += "<img src='images/ajax-loader.gif'></div>";
         insertHtml(selector, html);
     };
 
